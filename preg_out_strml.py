4f9ef6e89a0a5b9c87db1313f22700e97eeb53ca
Ibukun Olumodeji <olumodejiibukunayo@gmail.com>
2026-03-14 10:43:32 +0100

    Refactor pregnancy outcome prediction app layout

--- preg_out_strml.py
+++ preg_out_strml.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Pregnancy Outcome Predictor", layout="centered")
 st.markdown("""
 <style>
 .stApp {
-    background-color: #FFCOCB;
+    background-color: #FFC0CB;
     color: #FFFFFF;
 }
 div.stButton > button {
